e492d260487b0255d82152c1aab795e932042666
Uri Granta <uri.granta@hpe.com>
2017-07-31 08:59:43 +0100

    clean up femaleleaders slightly

--- dataviz/femaleleaders.py
+++ dataviz/femaleleaders.py
@@ -5,12 +5,8 @@ from records import *
 import seaborn as sns
 import dateparser
 
-# TODO: picture timeline
-
 rs = RecordCSV.load_file("femaleleaders.csv")
-rs = update_records(rs, update_if(lambda d: 'hosdate' in d, lambda d: update_in(d, ['hosdate'], lambda v: dateparser.parse(v).date())))
-rs = update_records(rs, update_if(lambda d: 'hogdate' in d, lambda d: update_in(d, ['hogdate'], lambda v: dateparser.parse(v).date())))
-rs = update_records(rs, update_if(lambda d: 'hosdate' in d or 'hogdate' in d, update_all(update_with(date=lambda d: min(d.get('hosdate',datetime.date.today()), d.get('hogdate',datetime.date.today()))), update_with(year=lambda d: d['date'].year))))
+rs = update_records(rs, update_if('hosdate:exists or hogdate:exists', update_with(date=lambda d: min(d.get('hosdate',2017), d.get('hogdate',2017)))))
 rd = records_to_dict(rs, "country")
 
 hog = ImageColor.from_floats(sns.color_palette("Blues", 6))
@@ -23,14 +19,14 @@ def stripe(c1, c2):
 def colorfn(c):
     if c not in rd: return None if c == 'Sea' else "grey"
     d = rd.get(c, {})
-    if 'year' not in d : return stripe("grey", hos[-1])
-    y = d.get('year') // 10 - 196
+    if 'date' not in d : return stripe("grey", hos[-1])
+    y = d.get('date') // 10 - 196
     if 'hog' not in d: return hos[y]
     elif 'hos' not in d: return hog[y]
     elif 'hosdate' not in d: return stripe(hog[y], both[y])
     else: return both[y]
     
-chart = map_chart("maps/Europe.png", colorfn, ignoring_exceptions(lambda c: str(rd[c]["year"])), label_font=arial(16, bold=True))
+chart = map_chart("maps/Europe.png", colorfn, ignoring_exceptions(lambda c: str(rd[c]["date"])), label_font=arial(16, bold=True))
 
 box_size = 30
 font_size = 16
@@ -38,7 +34,7 @@ font_size = 16
 def box(c): return Image.new("RGBA", (box_size,box_size), c)
 def stripebox(c1, c2): return Image.from_pattern(stripe(c1, c2), (box_size,box_size))
 
-type_boxes = ((box(hog[-1]), "premier"), (box(hos[-2]), "president"), (stripebox(hos[-2], "grey"), "queen"), (box(both[-2]), "premier & president"), (stripebox(hog[-2], both[-2]), "premier & queen"), (box("grey"), "none (yet)"))
+type_boxes = ((box(hog[-1]), "premier"), (box(hos[-2]), "president"), (stripebox(hos[-2], "grey"), "queen"), (box(both[-2]), "premier & president"), (stripebox(hog[-2], both[-2]), "premier & queen"), (box("grey"), "none (so far)"))
 type_arr = Image.from_array([[b, Image.from_text(label, arial(font_size), padding=(10,0))] for b,label in type_boxes], xalign=0, bg="white")
 type_leg = Image.from_column([Image.from_text("Office held", arial(font_size, bold=True)), type_arr], bg="white", xalign=0, padding=(0,5))
 
@@ -46,5 +42,8 @@ year_arr = Image.from_array([[box(hog[i]), box(hos[i]), box(both[i]), Image.from
 year_leg = Image.from_column([Image.from_text("First elected", arial(font_size, bold=True)), year_arr], bg="white", xalign=0, padding=(0,5))
 
 legend = Image.from_column([type_leg, year_leg], bg="white", xalign=0, padding=5).pad(1, "black")
-img = chart.place(legend, align=(1,0), padding=10)
-img.show()
+chart = chart.place(legend, align=(1,0), padding=10)
+
+title = Image.from_text("40 years of women leaders in Europe", arial(60, bold=True), "black", "white", padding=20)
+img = Image.from_column([title, chart], bg="white")
+img.save("femaleleaders.png")

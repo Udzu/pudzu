f8160dbde2966f213b948a0e1c0dcaffd77ac39b
Udzu <uri.zarfaty@gmail.com>
2017-08-03 21:49:57 +0100

    play around with pandas with a view to replacing records.py

---
+++ bamboo.py
@@ -0,0 +1,40 @@
+import pandas as pd
+
+from utils import *
+from records import *
+
+logger = logging.getLogger('bamboo')
+
+# Initially a translation of records.py to pandas. May turn into useful utilities.
+
+def df_keys(df):
+    return list(df.columns)
+    
+def df_values(df, key):
+    return remove_duplicates(df[key])
+    
+# TODO: recordfilter not working
+def filter_df(df, filter=None, key_filter=None):
+    if filter is not None:
+        record_filter = filter if callable(filter) else RecordFilter.make_filter(filter) 
+        df = df[df.apply(record_filter, axis=1)]
+    if key_filter is not None:
+        if callable(key_filter):
+            df = df.select(key_filter, axis=1)
+        else:
+            df = df.filter(make_iterable(key_filter), axis=1)
+    return df
+
+# TODO: progressbar
+def update_with(**kwargs):
+    return df.assign(**kwargs)
+
+# def update_if(condition, fn, alt_fn=None):
+
+# Testing
+
+rs = [ { "name": "Fred", "surname": "Flintstone", "children": 2 },
+       { "name": "Wilma", "surname": "Flintstone", "children": 2 },
+       { "name": "Dino", "children": 15 } ]
+df = pd.DataFrame(rs)
+

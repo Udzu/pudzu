4d26a117bd8f2ca06cf100a99527664e4dc6ce5d
Udzu <uri.zarfaty@gmail.com>
2017-08-05 14:33:30 +0100

    tidy up directory structure

--- dataviz/markovtext.py
+++ dataviz/markovtext.py
@@ -10,60 +10,44 @@ from records import *
 from charts import *
 from math import log
 
-CORPUS = "-wikienglish"
+CORPUS = "wikienglish"
 TITLE = "Letter and next-letter frequencies in English"
 SUBTITLE = "measured across 1 million sentences from Wikipedia"
+ENCODING = "utf-8"
+LETTERS = string.ascii_lowercase + ' '
 
-# CORPUS = "-wikifrench"
+# CORPUS = "wikifrench"
 # TITLE = "Letter and next-letter frequencies in French"
 # SUBTITLE = "measured across a selection of articles from Wikipedia"
+# ENCODING = "utf-8"
+# LETTERS = string.ascii_lowercase
 
-# CORPUS = "gutenberg"
-# TITLE = "Letter and next-letter frequencies in English"
-# SUBTITLE = "measured using the NLTK Gutenberg corpus"
+# Markov generators
 
-letters = string.ascii_lowercase + ' '
-corpusname, *corpusfiles = CORPUS.split("-")
-filebase = "markovtext_{}".format(CORPUS.lstrip("-"))
-
-def normalise(i):
-    return (nc for c in i for nc in unicodedata.normalize('NFKD', c).lower() if nc in letters)
-    
-def train_corpus(corpus, n):
+def load_generator(n):
     try:
-        logger.info("Load {}_{}.p".format(filebase, n))
-        with open("{}_{}.p".format(filebase, n), "rb") as f:
+        logger.info("Loading datasets/{}_{}.p".format(CORPUS, n))
+        with open("datasets/{}_{}.p".format(CORPUS, n), "rb") as f:
             return pickle.load(f)
     except:
-        logger.info("Training {} {}-grams".format(corpus, n))
+        logger.info("Training {} {}-grams".format(CORPUS, n))
         markov = MarkovGenerator(n)
-        if corpusname:
-            nltk_corpus = optional_import_from("nltk.corpus", corpusname)
-            for f in tqdm.tqdm(files if len(corpusfiles) > 0 else nltk_corpus.fileids()):
-                file = str(nltk_corpus.abspath(f))
-                encoding = nltk_corpus.encoding(f)
-                markov.train_file(file, encoding=encoding, normalise=normalise)
-        else:
-            for f in tqdm.tqdm(corpusfiles):
-                markov.train_file(f, encoding="utf-8", normalise=normalise)
-        logger.info("Save to {}_{}.p".format(filebase, n))
-        with open("{}_{}.p".format(filebase, n), "wb") as f:
+        for f in tqdm.tqdm(CORPUS.split("-")):
+            markov.train_file("datasets/"+f, encoding=ENCODING, normalise=partial(latin_normalise, letters=LETTERS))
+        logger.info("Saving to datasets/{}_{}.p".format(CORPUS, n))
+        with open("datasets/{}_{}.p".format(CORPUS, n), "wb") as f:
             pickle.dump(markov, f, pickle.HIGHEST_PROTOCOL)
         return markov
 
-g1 = train_corpus(CORPUS, 1)
-g2 = train_corpus(CORPUS, 2)
+g1 = load_generator(1)
+g2 = load_generator(2)
 
-# grid chart
+# Grid chart
+
 logger.info("Generating grid chart") 
-index = sorted([(x, g1.prob_dict[(x,)] / sum(g1.prob_dict.values())) for x in letters if (x,) in g1.prob_dict], key=lambda p: p[1], reverse=True)
+index = sorted([(x, g1.prob_dict[(x,)] / sum(g1.prob_dict.values())) for x in LETTERS if (x,) in g1.prob_dict], key=lambda p: p[1], reverse=True)
 array = [[(y,n / sum(g1.markov_dict[(x,)].values())) for y,n in g1.markov_dict[(x,)].most_common()] for x,_ in index]
 data = pd.DataFrame(array, index=index)
-
-csvarray = [["{} [{:.1%}]".format(*p) for p in r] for r in array]
-csvindex = ["{} [{:.1%}]".format(*p) for p in index]
-csvdata = pd.DataFrame(csvarray, index=csvindex)
-csvdata.to_csv("{}.csv".format(filebase))
 
 pone = ImageColor.from_floats(sns.color_palette("Reds", 8))
 ptwo = ImageColor.from_floats(sns.color_palette("Blues", 8))
@@ -83,7 +67,8 @@ def image_fn(pair, palette, row=None, size=40):
  
 grid = grid_chart(data, lambda p, r: image_fn(p, row=r, palette=ptwo), fg="black", bg="white", padding=1, row_label=lambda i: image_fn(data.index[i], palette=pone))
 
-# main legend
+# Main legend
+
 font_size = 18
 
 type_boxes = Image.from_array([
@@ -106,19 +91,14 @@ freq_leg = Image.from_column([Image.from_text("Letter frequencies", arial(font_s
 legend_inner = Image.from_column([type_leg, freq_leg], bg="white", xalign=0, padding=5)
 legend = legend_inner.pad(1, "black").pad((20,0,10,0), "white")
 
-# markov generated words
-def gen_words(gen):
-    while True:
-        word = "".join(gen.render(lambda o: len(o) > 1 and o[-1] == ' '))
-        if 12 >= len(word.strip()) >= 3 and word.startswith(" "):
-            yield word.strip()
+# Generated words
 
-if CORPUS == "-wikienglish":
+if CORPUS == "wikienglish":
     words = ["bastrabot", "dithely", "loctrion", "raliket", "calpereek", "amorth", "forliatitive", "asocult", "wasions", "quarm", "felogy", "winferlifterand", "sonsih", "uniso", "fourn", "hise", "meembege", "nuouish", "prouning", "guncelawits", "nown", "rectere", "abrip", "doesium"]
-elif CORPUS == "-wikifrench":
+elif CORPUS == "wikifrench":
     words = ["cillesil", "sulskini", "lidhemin", "plumeme", "bachogine", "crout", "taphie", "provicas", "copit", "odzzaccet", "extreiles", "pipiphien", "chetratagne", "outif", "suro", "extellages", "nans", "nutopune", "entote", "sporese", "zhiquis", "edes", "aliet", "randamelle"]
 else:
-    words = [w for w,_ in zip(gen_words(g2), range(24))]
+    words = [g2.render_word() for i in range(24)]
     
 word_array = Image.from_array([
     [Image.from_text(words[2*i], arial(font_size, italics=True), fg="black", bg="white"),
@@ -128,12 +108,13 @@ Image.from_text("The letters distributions in the chart can be used to generate 
 word_box = Image.from_column([word_title, word_array], bg="white", padding=5)
 word_box = word_box.pad_to_aspect(legend_inner.width, word_box.height, align=0, bg="white").pad(1, "white").pad((20,0,10,0), "white")
 
-# chart
+# Chart
+
 chart = Image.from_row([grid, legend], bg="white", yalign=0)
 chart = chart.place(word_box, align=1, padding=(0,75))
 title = Image.from_column([Image.from_text(TITLE, arial(40, bold=True), bg="white"),
 Image.from_text(SUBTITLE, arial(24, bold=True), bg="white")])
 full = Image.from_column([title, chart], bg="white", padding=(0,10))
 full.place(Image.from_text("/u/Udzu", font("arial", 16), fg="black", bg="white", padding=5).pad((1,1,0,0), "black"), align=1, padding=10, copy=False)
-full.save("{}.png".format(filebase))
+full.save("output/{}.png".format(CORPUS))
 

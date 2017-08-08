dd46dcf02f53de36809eaef21087b7f405552b43
Uri Granta <uri.granta@hpe.com>
2017-08-08 15:44:10 +0100

    [bamboo, utils] convert one more dataviz to pandas

--- bamboo.py
+++ bamboo.py
@@ -11,39 +11,42 @@ logger = logging.getLogger('bamboo')
 
 # Various pandas utility functions
 
+def _key_filter(df, filter):
+    """Filter rows using either a list of keys or a key predicate."""
+    return df.select(filter, axis=1) if callable(filter) else df.filter(make_iterable(filter), axis=1)
+
 def _row_filter(df, filter):
     """Filter rows using either a row predicate or a RecordFilter expression."""
     df_filter = filter if callable(filter) else FilterExpression.make_filter(filter) 
     return df[df.apply(df_filter, axis=1)]
 
-def _key_filter(df, filter):
-    """Filter rows using either a list of keys or a key predicate."""
-    return df.select(filter, axis=1) if callable(filter) else df.filter(make_iterable(filter), axis=1)
-
 def _row_assign(df, assign_if=None, **kwargs):
     """Assign or update columns using row function, with an optional row predicate condition."""
     return df.assign(**{k : (lambda df: [fn(r) if (assign_if is None or assign_if(r)) else r.get(k) for _,r in df.iterrows()]) for k,fn in kwargs.items()})
 
-def _column_update(df, update_if=None, **kwargs):
-    """Update columns using a value function, with an optional value predicate condition."""
-    return df.assign(**{k : (lambda df: [fn(r[k]) if (update_if is None or update_if(r[k])) else r.get(k) for _,r in df.iterrows()]) for k,fn in kwargs.items()})
-    
 def _row_groupby(df, by):
     """Group rows using a row function, map, list or column name."""
     return df.groupby(lambda i: by(df.ix[i]) if callable(by) else by[i] if non_string_iterable(by) else df.ix[i].get(by))
 
-def _column_split(df, by):
+def _row_split(df, by):
     """Split rows by column, making one copy for each item in the column value."""
     return pd.DataFrame(pd.Series(assoc_in(row, [by], v)) for _, row in df.iterrows() for v in make_iterable(row[by]))
 
-# todo: tabulation
+def _column_update(df, update_if=None, **kwargs):
+    """Update columns using a value function, with an optional value predicate condition."""
+    return df.assign(**{k : (lambda df, k=k, fn=fn: [fn(r[k]) if (update_if is None or update_if(r[k])) else r.get(k) for _,r in df.iterrows()]) for k,fn in kwargs.items()})
+    
+def _column_split(df, columns, delimiter):
+    """Split column values into lists with the given delimiter."""
+    return df.update_columns(**{column : ignoring_exceptions(lambda s: tuple(s.split(delimiter)), (), (AttributeError)) for column in columns })
     
 pd.DataFrame.filter_keys = _key_filter
 pd.DataFrame.filter_rows = _row_filter
-pd.DataFrame.assign_row = _row_assign
-pd.DataFrame.groupby_row = _row_groupby
-pd.DataFrame.update_column = _column_update
-pd.DataFrame.split_column = _column_split
+pd.DataFrame.assign_rows = _row_assign
+pd.DataFrame.groupby_rows = _row_groupby
+pd.DataFrame.split_rows = _row_split
+pd.DataFrame.update_columns = _column_update
+pd.DataFrame.split_columns = _column_split
 
 # filter expressions
 
@@ -79,7 +82,7 @@ class FilterExpression:
     str_ops = { '=': lambda x,y: x==str(y), '!=': lambda x,y: x!=str(y),
                 '~': lambda x,y: re.search(y,str(x)), '!~': lambda x,y: not re.search(y,str(x)),
                 '>>': lambda x,y: y in x, '!>>': lambda x,y: y not in x }
-    exist_ops = { ':': lambda x,y: { 'exists': not (isinstance(x, float) and math.isnan(x)), 'true': bool(x) }[y.lower()] }
+    exist_ops = { ':': lambda x,y: { 'exists': not none_or_nan(x), 'true': bool(x) }[y.lower()] }
                 
     def oneOfOpMap(map): return oneOf(list(map.keys())).setParseAction(lambda t: ignoring_exceptions(map[t[0]], False))
     num_op = oneOfOpMap(num_ops)

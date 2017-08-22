c6d044fb8e78e3ce37655776b85ae2f6104f2f17
Udzu <uri.zarfaty@gmail.com>
2017-08-22 21:15:43 +0100

    [dataviz] initial wikifame data collection

--- dataviz/wikifame.py
+++ dataviz/wikifame.py
@@ -7,7 +7,7 @@ harmonic_mean = optional_import_from('statistics', 'harmonic_mean', lambda data:
 
 # collect data
 
-def parse_wiki_births(year):
+def extract_births(year):
     DATE_PATTERN = re.compile(r"^[_ 0-9]*(January|February|March|April|May|June|July|August|September|October|November|December)[ 0-9]*$")
     wp = WikiPage.from_year(year)
     h2_start = find_tags(wp.bs4, all_(string='Births'), parents_("h2"))
@@ -17,7 +17,7 @@ def parse_wiki_births(year):
                               exclude_(h2_end, is_after),
                               exclude_(h2_start, is_before))
     links = remove_duplicate_tags(links)
-    return pd.DataFrame([WikiPage.title_from_url(a['href']) for a in links], columns=["link"])
+    return pd.DataFrame([{ "year": year, "link": WikiPage.title_from_url(a['href'])} for a in links])
     
 def score_people(df):
     LIMITS = { 'length': 1500000, 'revisions': 25000, 'pageviews': 1000000 }
@@ -28,7 +28,17 @@ def score_people(df):
                         length=lambda d: len(d['wp'].response.content),
                         pageviews=lambda d: int(median(([pv['views'] for pv in d['wp'].pageviews("20160101", "20170101")]+[0]*12)[:12])),
                         revisions=lambda d: d['wp'].revision_count(),
-                        disambiguation=lambda d: d['wp'].bs4.find(alt="Disambiguation icon") and True)
+                        disambiguation=lambda d: bool(d['wp'].bs4.find(alt="Disambiguation icon")))
     df = df.assign_rows(score=lambda d: harmonic_mean([log(max(d[k], 2)) / log(max_value) for k,max_value in LIMITS.items()]))
-    return df
+    return df.filter_columns(lambda k: k != 'wp')
 
+def score_births(years):
+    dfs = [score_people(extract_births(year)) for year in tqdm.tqdm(years)]
+    df = pd.concat(dfs, ignore_index=True).sort_values('score', ascending=False)
+    df.to_csv("datasets/wikibirths/{}-{}.csv".format(min(years), max(years)), index=False, encoding="utf-8")
+    return df
+    
+def score_births_by_decade(decades):
+    for d in tqdm.tqdm(decades):
+        ignoring_exceptions(score_births)(range(d*10,d*10+10))
+            

be15426537b2ce544577b2aadfbfd755432bc230
Uri Granta <uri.granta@hpe.com>
2017-08-24 11:54:49 +0100

    [dataviz] more wikibirths

--- dataviz/wikifame.py
+++ dataviz/wikifame.py
@@ -5,29 +5,31 @@ sys.path.append('..')
 from charts import *
 from wikipage import *
 
-harmonic_mean = optional_import_from('statistics', 'harmonic_mean', lambda data: len(data) / sum(1/x for x in data))
+# data visualisation 
 
-# data visualisation (WIP)
-
-CMAP = sns.cubehelix_palette(start=.5, rot=-.75, as_cmap=True)
 DEFAULT_IMG = "https://s-media-cache-ak0.pinimg.com/736x/0d/36/e7/0d36e7a476b06333d9fe9960572b66b9.jpg"
-
 df = pd.read_csv("datasets/wikibirths.csv")
 table = pd.DataFrame([[df.iloc[century*10+decade]['name'] if century*10+decade < len(df) else None for decade in range(0,10)] for century in range(0,10)],
                      index=["{}00s".format(c) for c in range(10,20)], columns=["'{}0s".format(d) for d in range(0,10)])
 df = df.set_index('name')
-df = df.assign_rows(image_url=lambda d: WikiPage(d.name).image_url, progressbar=True)
 
 def process(img, name):
-    bg = ImageColor.from_floats(CMAP((1 - df['score'][name]) * 2))
+    bg = "black"
     box = Image.new("RGB", (180,200), bg)
     box = box.place(Image.from_column([
       img.crop_to_aspect(100, 100, (0.5, 0.2)).resize_fixed_aspect(width=160),
-      Image.from_text(name, arial(12, bold=True), padding=(3, 5, 3, 0), fg="white", bg=bg)
+      Image.from_text(name, arial(12, bold=True), padding=(3, 5, 3, 2), fg="white", bg=bg),
+      Image.from_text(get_non(df['description'], name, ""), arial(12), padding=(3,0,3,0), fg="white", bg=bg)
       ], bg=bg))
     return box
     
-grid = grid_chart(data, lambda d: d['image_url'] or DEFAULT_IMG, image_process=process, row_label=arial(20, bold=True), col_label=arial(20, bold=True), bg="black")
+title = Image.from_column([
+Image.from_text("100 famous people of the second millennium", arial(60, bold=True), fg="white", bg="black").pad((10,0)),
+Image.from_text("the most famous person born each decade, according to English Wikipedia", arial(36, bold=True), fg="white", bg="black").pad((10,0,10,2))
+], bg="black").pad((0,10))
+
+grid = grid_chart(table, lambda n: n and get_non(df['image_url'], n, DEFAULT_IMG), image_process=process, row_label=arial(20, bold=True), col_label=arial(20, bold=True), bg="black", title=title)
+grid.show()
 
 # data collection (would need more cleanup/corroboration to be used in larger quantities)
 
@@ -45,6 +47,7 @@ def extract_births(year):
     
 def score_people(df):
     LIMITS = { 'length': 1500000, 'revisions': 25000, 'pageviews': 1000000 }
+    harmonic_mean = optional_import_from('statistics', 'harmonic_mean', lambda data: len(data) / sum(1/x for x in data))
     df = df.assign_rows(progressbar = True,
                         wp=lambda d: WikiPage(d['link']))
     df = df.assign_rows(progressbar = True,

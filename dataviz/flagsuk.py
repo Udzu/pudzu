44aa6f93b3c2160b22bfc11ead219aa340b35449
Uri Granta <uri.granta@hpe.com>
2017-10-23 16:59:04 +0100

    [dataviz] another flag list

---
+++ dataviz/flagsuk.py
@@ -0,0 +1,28 @@
+import sys
+sys.path.append('..')
+from charts import *
+from bamboo import *
+
+df = pd.read_csv("datasets/flagsuk.csv")
+fg, bg="black", "#EEEEEE"
+default_img = "https://s-media-cache-ak0.pinimg.com/736x/0d/36/e7/0d36e7a476b06333d9fe9960572b66b9.jpg"
+
+array = [[
+    Image.from_column([
+        Image.from_text(d['country'], arial(48, bold=True), fg=fg, bg=bg, align="center", max_width=400),
+        Image.from_text("({})".format(d['dates']), arial(48, bold=False), fg=fg, bg=bg)
+    ], bg=bg, padding=(0,5)),
+    Image.from_url_with_cache(d['flag']).resize((320,200)),
+    Image.from_text(get_non(d, 'description', 'f'), arial(48), fg=fg, bg=bg, max_width=1000, padding=(0,4,0,8))
+    ]
+    for _, d in df.iterrows()]
+
+flags =  Image.from_array(array, bg=bg, padding=(10,20), xalign=(0.5,0,0)).pad((5,0), bg=bg)
+
+title = Image.from_column([
+    Image.from_text("Variations on a theme".upper(), arial(72, bold=True), fg=fg, bg=bg),
+    Image.from_text("8 flags that are based on the Union Jack (and 1 that isn't) ", arial(48, italics=True), fg=fg, bg=bg).pad(5,bg=bg)
+    ], bg=bg)
+      
+img = Image.from_column([title, flags], bg=bg, padding=(0,10))
+img.save("output/flagsuk.png")

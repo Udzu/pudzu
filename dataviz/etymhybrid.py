3b8948d3e9a24233ddb852a57d0d6c2ecdeee9a4
Uri Granta <uri.granta@hpe.com>
2017-11-01 11:28:28 +0000

    [dataviz] hybrid words

---
+++ dataviz/etymhybrid.py
@@ -0,0 +1,25 @@
+import sys
+sys.path.append('..')
+from charts import *
+from bamboo import *
+
+COLS = ["TOPIC", "DOODAH", "WHATSIT" ]
+
+TFG, TBG = "white", "#00008B"
+MFG, MBG = "black", "#F5F5DC"
+GFG, GBG = "#F5F5DC", "black"
+
+df = pd.read_csv("datasets/etymhybrid.csv")
+df.columns = COLS
+
+title = Image.from_text_dynamic("Linguistic meme template #7".upper(), 400, 36, papply(arial, bold=True), fg=TFG, bg=TBG, padding=10)
+title = title.pad_to_aspect(400, title.height, bg=TBG)
+
+description = Image.from_text_dynamic("\n{} make(s) me sick!\n\nIt should be either {} or {}.\nMixing Greek and Latin roots is wrong!\n".format(*COLS), 400, 36, papply(arial, bold=True), fg=MFG, bg=MBG, padding=10, line_spacing=10)
+description = description.pad_to_aspect(400, description.height, bg=MBG)
+
+grid = grid_chart(df, lambda s: Image.from_text(s, arial(16, bold=True), GFG, GBG), padding=(10,4), col_label=arial(16, bold=True), bg=GBG, fg=GFG).pad((0,0,0,4), GBG)
+grid = grid.pad_to_aspect(400, grid.height, bg=GBG)
+
+img = Image.from_column([title, description, grid])
+img.save("output/etymhybrid.png")

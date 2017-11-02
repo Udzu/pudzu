b213ece1887d17a6c1bd50978159b67db58fe384
Uri Granta <uri.granta@hpe.com>
2017-11-02 14:07:24 +0000

    [dataviz] s**t etymology fixes

--- dataviz/etymshit.py
+++ dataviz/etymshit.py
@@ -9,35 +9,30 @@ df = pd.read_csv("datasets/etymshit.csv").split_columns("group", "|").set_index(
 
 CATEGORIES = [
 ("kakka", 'from Proto-Indo-European *kakka- ("defecate")'),
-("strent", 'from Proto-Indo-European *(s)terǵ- ("dirt, dung")'),
+# ("strent", 'from Proto-Indo-European *(s)terǵ- ("stiff")'),
+("sḱer", 'from Proto-Indo-European *sḱer- ("dirt, dung")'),
 ("skita", 'from Proto-Germanic *skīta- ("to separate")'),
 ("merda", 'from Proto-Italic *(s)merdā ("stench")'),
-("govьno", 'from Proto-Slavic *govьno ("excrement")'),
-("lajьno", 'from Proto-Slavic **lajьno ("flax?")'),
+("govьno", 'from Proto-Slavic *govьno ("cow dung")'),
+("lajьno", 'from Proto-Slavic *lajьno ("flax?")'),
 ("paska", 'from  Proto-Finno-Ugric *pućka ("excrement?")'),
-("suds", 'from Proto-Baltic??'),
+("suds", 'from Proto-Baltic ??'),
 ("other", "\n".join([
 'bok: from Proto-Turkic *bok ("dirt, dung")',
 'hara: from Arabic ḵarāʾ ("defecate")',
 'lort: from Proto-Germanic *lurtaz ("crooked")',
 'mut: from Proto-Indo-European *meu ("moist")',
-'rahat: from Turkish rahat ("delight")',
+'rahat: from Turkish rahat ("comfortable")',
+'srati: from Proto-Slavic sьrati ("dung")',
 'szar: from Proto-Finno-Urgic *śarɜ ("to dry?")',
+'stront: from Proto-Indo-European *(s)terǵ- ("stiff")',
 'tifa: from Latin pasta ("paste") via pastifarada?'
 ]))
-## ("śarɜ", 'from Proto-Finno-Urgic *śarɜ ("to dry?")'),
-## ("hara", 'from Arabic ḵarāʾ ("defecate")'),
-## ("rāḥa", 'from Turkish rahat ("delight")'),
-## ("mut", 'from Proto-Indo-European *meu ("moist")'),
-## ("bok", 'from Proto-Turkic *bok ("dirt, dung")'),
-## ("pasta", 'possibly from Latin pasta ("paste")'),
-## ('lurtaz', 'from Proto-Germanic *lurtaz ("crooked")'),
 ]
 
 GROUPS = [n for n,_ in CATEGORIES]
-#COLORS = ImageColor.from_floats(sns.cubehelix_palette(15, start=-1.5, rot=1.5, light=0.85, dark=0.15))
 COLORS = ImageColor.from_floats(sns.color_palette("Set2", len(CATEGORIES)))
-COLORS[-1], COLORS[7] = COLORS[7], COLORS[-1]
+COLORS[-1], COLORS[7] = COLORS[7], COLORS[-1] # make grey last
 
 def catcol(cat):
     return COLORS[-1] if cat not in GROUPS else COLORS[GROUPS.index(cat)]
@@ -66,15 +61,12 @@ def box(c, shape=(25,25)): return Image.new("RGBA", shape, c)
 def boxtext(s): return Image.from_text(s, arial(14), padding=(10,0,0,3), align="left")
 def sectiontext(s): return Image.from_text(s, arial(16, bold=True))
 
-boxes = Image.from_array([[box(COLORS[i],(25,25 if i+1<len(CATEGORIES) else 100)), boxtext(s)] for i,(_,s) in enumerate(CATEGORIES)], bg="white", xalign=0)
-
-section = boxes # Image.from_column([sectiontext("Name origin"), boxes], bg="white", xalign=0, padding=(0,5))
-
+section = Image.from_array([[box(COLORS[i],(25,25 if i+1<len(CATEGORIES) else 120)), boxtext(s)] for i,(_,s) in enumerate(CATEGORIES)], bg="white", xalign=0)
 legend = Image.from_column([section], bg="white", xalign=0, padding=10).pad(1, "black")
 chart = map.place(legend, align=(1,0), padding=10)
 
 title = Image.from_column([
-Image.from_text("A shit by any other name...".upper(), arial(48, bold=True)),
+Image.from_text("A shit by any other name?".upper(), arial(48, bold=True)),
 Image.from_text("the etymology of shit in different European languages", arial(36))],
 bg="white")
 

a9c18808bacd3f0803bc6db463494c38ecc4d959
Udzu <uri.zarfaty@gmail.com>
2017-11-27 15:52:57 +0000

    [charts] tidy up time_chart

--- dataviz/politics_g7.py
+++ dataviz/politics_g7.py
@@ -19,9 +19,18 @@ def percentage_left(df):
     
 groups = pd.read_csv("datasets/g7.csv").groupby_rows(lambda d: "{} ({})".format(d['country'], d['office']))
 group_order = sorted(list(groups.groups), key=lambda s: percentage_left(groups.get_group(s)), reverse=True)
+data = [groups.get_group(g) for g in group_order]
 
 colorfn = lambda d: {"left": "#d62728", "right": "#393b79", "centre": "#e7ba52"}[d['spectrum']]
-chart = time_chart(groups, lambda d: dateparser.parse(d['start']).date(), lambda d: dateparser.parse(get_non(d, 'end', END.isoformat())).date(), colorfn, 1200, 40, xmin=START, group_labels=arial(16), group_order=group_order, group_info=lambda g,r: "{:.0%}".format(percentage_left(r)), title=Image.from_text("G7 countries by time spent under left-of-centre governments (1960-present)", arial(30, bold=True), fg="white").pad((0,5,0,30),bg="black"), grid_interval=DateInterval(years=10), element_images=lambda d: Image.from_text(d['name'].split(" ")[-1], arial(10), padding=(5,2), fg="white", bg=colorfn(d)), grid_labels=arial(16), label_format=lambda v: str(v.year)).pad(5, "black")
+startfn = lambda d: dateparser.parse(d['start']).date()
+endfn = lambda d: dateparser.parse(get_non(d, 'end', END.isoformat())).date()
+labelfn = lambda d: Image.from_text(d['name'].split(" ")[-1], arial(10), padding=(5,2), fg="white", bg=colorfn(d))
+labels = ["{:.0%}".format(percentage_left(df)) for df in data]
+title = Image.from_text("G7 countries by time spent under left-of-centre governments (1960-present)", arial(30, bold=True), fg="white").pad((0,5,0,30),bg="black")
+
+chart = time_chart(data, 1200, 40, startfn, endfn, colorfn, element_images=labelfn,
+                   xmin=START, label_font=arial(16), labels_left=group_order, labels_right=labels, title=title,
+                   grid_interval=DateInterval(years=10), grid_font=arial(16), grid_labels=lambda v: str(v.year)).pad(5, "black")
 
 def box(s): return Image.new("RGBA", (20,20), colorfn({"spectrum": s}))
 def label(s): return Image.from_text(s, arial(12), fg="white")

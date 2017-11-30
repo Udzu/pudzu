721a262986af385689bf7ea1fdb1bd434c1f8ec4
Uri Granta <uri.granta@hpe.com>
2017-11-30 14:39:01 +0000

    [dataviz] jerusalem

--- dataviz/timeline_jerusalem.py
+++ dataviz/timeline_jerusalem.py
@@ -3,39 +3,49 @@ sys.path.append('..')
 from charts import *
 from dates import *
 
-# -------------
-# G7 time chart
-# -------------
-
 df = pd.read_csv("datasets/timeline_jerusalem.csv").split_columns("type", "|")
 
 START, END = -2000, 2000
 INTERVAL = 500
 COLORS = { "christian": "#e41a1c", "pagan": "#ff7f00", "jewish": "#377eb8", "muslim": "#4daf4a" }
+PHOTOS = ["https://media2.s-nbcnews.com/j/newscms/2017_20/2001786/170516-western-wall-mn-1100_9029fb3cafb1eb2f3a65fbb066ddbc0e.nbcnews-ux-2880-1000.jpg", "https://1gr.cz/fotky/idnes/15/041/org/HIG5a5131_04.jpg", "http://www.hexapolis.com/wp-content/uploads/2014/07/Dome-of-the-Rock_1.jpg"]
 
 def stripes(colors): return Image.from_column([Image.new("RGBA", (100, 4), c) for c in colors])
 
-labelfn = lambda d, w: " " if ("Herod" in d['period'] or "Mamluk" in d['period']) and w < 50 else d['period']
+def labelfn(d, w): # hack to handle 500 year cutoffs
+    if "Herod" in d['period'] and w < 50: return " "
+    elif "Achaemenid" in d['period'] and w < 200: return "8→"
+    elif "Fatimid" in d['period'] and w < 100: return "26→"
+    elif "Mamluk" in d['period'] and w < 50: return "33"
+    return d['period'].replace("\\n", "\n")
+    
 colorfn = lambda d, w, h: Image.from_pattern(stripes([COLORS[t] for t in d['type']]), (w, h))
-
 labeld = {frozenset({TimeChartLabelPosition.ABOVE, TimeChartLabelPosition.INSIDE, TimeChartLabelPosition.BELOW}): labelfn,
           TimeChartLabelPosition.ABOVE: " ", TimeChartLabelPosition.BELOW: " "}
 data = [df.filter_rows("start<{} and end>{}".format(start+INTERVAL, start)).update_columns(start=lambda v: v-start, end=lambda v: v-start) for start in range(START, END, INTERVAL)]
 llabels = ["{} BC".format(-start) if start< 0 else "AD {}".format(start+int(start==0)) for start in range(START, END, INTERVAL)]
-
 chart = time_chart(data, 800, 40, "start", "end", colorfn, labels=labeld, label_font=arial(10), 
                    xmin=0, xmax=INTERVAL, grid_font=arial(10), grid_labels=lambda v: "+{}".format(v), grid_interval=100, labels_left=llabels).pad(2, bg="black")
 
-chart.save("output/timeline_jerusalem.png")
+# Title, etc
 
-# def duration(d):
-    # return END - max(START, dateparser.parse(d['start']).date())
-    
-# def percentage_left(df):
-    # return sum((duration(d) for _,d in df[df.spectrum == "left"].iterrows()), datetime.timedelta(0)) / sum((duration(d) for _,d in df.iterrows()), datetime.timedelta(0))
-    
-# groups = pd.read_csv("datasets/g7.csv").groupby_rows(lambda d: "{} ({})".format(d['country'], d['office']))
-# group_order = sorted(list(groups.groups), key=lambda s: percentage_left(groups.get_group(s)), reverse=True)
+images = Image.from_column([Image.from_url_with_cache(u).crop_to_aspect(320,200).resize((320,200)) for u in PHOTOS], bg="black")
 
-# colorfn = lambda d: {"left": "#d62728", "right": "#393b79", "centre": "#e7ba52"}[d['spectrum']]
-# chart = time_chart(groups, lambda d: dateparser.parse(d['start']).date(), lambda d: dateparser.parse(get_non(d, 'end', END.isoformat())).date(), colorfn, 1200, 40, xmin=START, group_labels=arial(16), group_order=group_order, group_info=lambda g,r: "{:.0%}".format(percentage_left(r)), title=Image.from_text("G7 countries by time spent under left-of-centre governments (1960-present)", arial(30, bold=True), fg="white").pad((0,5,0,30),bg="black"), grid_interval=DateInterval(years=10), element_images=lambda d: Image.from_text(d['name'].split(" ")[-1], arial(10), padding=(5,2), fg="white", bg=colorfn(d)), grid_labels=arial(16), label_format=lambda v: str(v.year)).pad(5, "black")
+def box(t): return Image.new("RGBA", (20,20), COLORS[t])
+def label(s): return Image.from_text(s, arial(12), fg="white", bg="black")
+
+footer = Image.from_row([
+    Image.from_text("ruling entity by religion: ", arial(12, bold=True), fg="white"),
+    box("pagan"), label("pagan"), box("jewish"), label("Jewish"), box("christian"), label("Christian"), box("muslim"), label("Muslim"),
+    Image.new("RGBA", (50,0), 0),
+    Image.from_text("* also claimed (but not controlled) by the State of Palestine", arial(12), fg="white"),
+    ], padding=3, bg="black")
+    
+title = Image.from_column([
+    Image.from_text("Jerusalem: 40 states in 4000 years".upper(), arial(36, bold=True), fg="white", bg="black"),
+    Image.from_text("a political timeline of the Holy City, based on http://qr.ae/TbcqnX", arial(24), fg="white", bg="black")
+    ], bg="black")
+
+img = Image.from_column([title, Image.from_row([chart, images], bg="black", padding=5), footer], bg="black", padding=(0,10))
+img.place(Image.from_text("/u/Udzu", font("arial", 12), fg="white", bg="black", padding=3).pad((1,1,0,0), "white"), align=1, padding=(5,10), copy=False)
+img.save("output/timeline_jerusalem.png")

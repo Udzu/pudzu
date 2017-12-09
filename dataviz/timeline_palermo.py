9bf61314980cce6ae4600344e4287f54676e5a95
Udzu <uri.zarfaty@gmail.com>
2017-12-09 15:10:08 +0000

    [dataviz] palermo and capitals cleanup

--- dataviz/timeline_palermo.py
+++ dataviz/timeline_palermo.py
@@ -3,19 +3,28 @@ sys.path.append('..')
 from charts import *
 from dates import *
 
-df = pd.read_csv("datasets/timeline_palermo.csv")
+df = pd.read_csv("datasets/timeline_palermo.csv").split_columns("type", "|")
 
 START, END = -1000, 2000
 INTERVAL = 500
-COLORS = { "classical": "#e41a1c", "german": "#ff7f00", "spanish": "#377eb8", "french": "#4daf4a" }
-# PHOTOS = ["https://media2.s-nbcnews.com/j/newscms/2017_20/2001786/170516-western-wall-mn-1100_9029fb3cafb1eb2f3a65fbb066ddbc0e.nbcnews-ux-2880-1000.jpg", "https://1gr.cz/fotky/idnes/15/041/org/HIG5a5131_04.jpg", "http://www.hexapolis.com/wp-content/uploads/2014/07/Dome-of-the-Rock_1.jpg"]
+COLORS = { 
+"classical": "#984ea3", 
+"italian": "#e41a1c",
+"spanish": "#ff7f00",
+"french": "#377eb8",
+"german": "#999999", 
+"semitic": "#4daf4a"
+ }
+PHOTOS = ["http://www.sicily.co.uk/wp-content/uploads/palermo-body-11-3000x1500.jpg", "https://pbs.twimg.com/media/C3kUcTRWEAEpwXN.jpg"]
 
 def stripes(colors): return Image.from_column([Image.new("RGBA", (100, 4), c) for c in colors])
 
 def labelfn(d, w):
+    if "Ostro" in d['period'] and w < 70: return "..."
+    if "of Aragon" in d['period'] and w < 50: return "..."
     return d['period'].replace("\\n", "\n")
     
-colorfn = lambda d, w, h: Image.from_pattern(stripes([COLORS['classical']]), (w, h))
+colorfn = lambda d, w, h: Image.from_pattern(stripes([COLORS[t] for t in d['type']]), (w, h))
 labeld = {frozenset({TimeChartLabelPosition.ABOVE, TimeChartLabelPosition.INSIDE, TimeChartLabelPosition.BELOW}): labelfn,
           TimeChartLabelPosition.ABOVE: " ", TimeChartLabelPosition.BELOW: " "}
 data = [df.filter_rows("start<{} and end>{}".format(start+INTERVAL, start)).update_columns(start=lambda v: v-start, end=lambda v: v-start) for start in range(START, END, INTERVAL)]
@@ -25,23 +34,28 @@ chart = time_chart(data, 800, 40, "start", "end", colorfn, labels=labeld, label_
 
 # # Title, etc
 
-# images = Image.from_column([Image.from_url_with_cache(u).crop_to_aspect(320,200).resize((320,200)) for u in PHOTOS], bg="black")
+images = Image.from_column([Image.from_url_with_cache(u).crop_to_aspect(320,200).resize((320,200)) for u in PHOTOS], bg="black")
 
-# def box(t): return Image.new("RGBA", (20,20), COLORS[t])
-# def label(s): return Image.from_text(s, arial(12), fg="white", bg="black")
+def box(t): return Image.new("RGBA", (20,20), COLORS[t])
+def label(s): return Image.from_text(s, arial(12), fg="white", bg="black", padding=(0,0,10,0))
 
-# footer = Image.from_row([
-    # Image.from_text("ruling entity by religion: ", arial(12, bold=True), fg="white"),
-    # box("pagan"), label("pagan"), box("jewish"), label("Jewish"), box("christian"), label("Christian"), box("muslim"), label("Muslim"),
-    # Image.new("RGBA", (50,0), 0),
-    # Image.from_text("* also claimed (but not controlled) by the State of Palestine", arial(12), fg="white"),
-    # ], padding=3, bg="black")
+footer = Image.from_row([
+    Image.from_text("ruling power: ", arial(12, bold=True), fg="white"),
+    box("semitic"), label("Semitic"),
+    box("classical"), label("Graeco-Roman"),
+    box("german"), label("Germanic"),
+    box("french"), label("Gallic"),
+    box("spanish"), label("Hispanic"),
+    box("italian"), label("Italic")
+    ], padding=3, bg="black")
     
-# title = Image.from_column([
-    # Image.from_text("Jerusalem: 40 states in 4000 years".upper(), arial(36, bold=True), fg="white", bg="black"),
-    # Image.from_text("a political timeline of the Holy City, based on http://qr.ae/TbcqnX", arial(24), fg="white", bg="black")
-    # ], bg="black")
+chart = Image.from_column([chart, footer.pad((0,10))], bg="black")
 
-# img = Image.from_column([title, Image.from_row([chart, images], bg="black", padding=5), footer], bg="black", padding=(0,10))
-# img.place(Image.from_text("/u/Udzu", font("arial", 12), fg="white", bg="black", padding=3).pad((1,1,0,0), "white"), align=1, padding=(5,10), copy=False)
-# img.save("output/timeline_jerusalem.png")
+title = Image.from_column([
+    Image.from_text("Palermo: most conquered city(?)".upper(), arial(36, bold=True), fg="white", bg="black"),
+    Image.from_text("a political chronology of Sicily's ancient, oft conquered capital", arial(24), fg="white", bg="black")
+    ], bg="black")
+
+img = Image.from_column([title.pad((0,10)), Image.from_row([chart, images], bg="black", padding=5)], bg="black")
+img.place(Image.from_text("/u/Udzu", font("arial", 12), fg="white", bg="black", padding=3).pad((1,1,0,0), "white"), align=1, padding=(5,10), copy=False)
+img.save("output/timeline_palermo.png")

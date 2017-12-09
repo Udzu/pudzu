21cd155005de6961f91ffdad6dc7912d912f5ce4
Udzu <uri.zarfaty@gmail.com>
2017-12-09 15:10:08 +0000

    [dataviz] palermo and capitals cleanup

--- dataviz/euuscapitals.py
+++ dataviz/euuscapitals.py
@@ -32,7 +32,7 @@ def box(population):
     return choose(population, [square, circle, cross])(20, COLOR).resize_fixed_aspect(scale=0.25)
     
 def label(name, population):
-    size = choose(population, [24, 20, 18])
+    size = choose(population, [28, 24, 20])
     bold = choose(population, [True, True, False])
     return Image.from_text(name, arial(size, bold=bold), COLOR, align="center", padding=(0,0,0,4))
     
@@ -53,7 +53,7 @@ def add_cities(img, cities):
 map = add_cities(map, df)
 
 lpops = [100000, 10000, 1000]
-lfooter = "\n• ".join(["Map is limited to places with 1000+ residents. Smaller locations include:\n", "Copenhagen NY (pop 801)", "Oslo MN (pop 330)", "Angora Township MN (pop 249)", "Kief ND (pop 13)", "Reykjavik MB (small community)", "Vatican LA (small community)", "Sofia NM (ghost town)", "Budapest GA (ghost town)"]) + "\n\nThere is also an Agram Township MN (pop 534), named after Zagreb's old German name."
+lfooter = "\n• ".join(["Map is limited to places with 1000+ residents and shows only the largest example for each name. Smaller locations that were omitted include:\n", "Copenhagen NY (pop 801)", "Oslo MN (pop 330)", "Kief ND (pop 13)", "Reykjavik MB (small community)", "Vatican LA (small community)", "Sofia NM (ghost town)", "Budapest GA (ghost town)"]) + "\n\nThere is also an Agram Township MN (pop 534), named using Zagreb's old German name, and an Angora Township MN (pop 249), named using Ankara's old English name."
 lheader = "By population"
 legend = generate_legend([box(p) for p in lpops], [label(" {:,}+ residents".format(p), p) for p in lpops], footer=lfooter, header=lheader, max_width=300)
 
@@ -63,13 +63,13 @@ nfooter = "\n• ".join(["Pronunciations are as expected, with the following exc
 "Prague is PRAYG, not PRAHG.",
 "Riga is RYE-guh, not REE-guh.\n",
 "(Athens is pronounced normally, unlike Athens IL or Athens KY, which are pronounced AY-thens)",
-"(Vienna is pronounced normally, unlike Vienna IL or Vienna SD, which are pronounced vie-EH-nuh)"
+"(Vienna is pronounced normally, unlike Vienna IL or Vienna SD, which are pronounced vie-eh-nuh)"
 ])
 note = generate_legend([], [], footer=nfooter, header="By pronunciation", max_width=280)
 
 img = Image.from_row([map, Image.from_column([legend, note.pad((0,20,0,0), "white")], bg="white", xalign=0)], yalign=0, bg="white", padding=10)
 
-title = Image.from_text("Capital cities of Europe".upper(), arial(60, bold=True))
+title = Image.from_text("Capital cities of Europe: North American edition".upper(), arial(60, bold=True))
 img = Image.from_column([title, img], bg="white", padding=(0,10))
 img.place(Image.from_text("/u/Udzu", font("arial", 16), fg="black", bg="white", padding=5).pad((1,1,0,0), "black"), align=1, padding=10, copy=False)
 img.save("output/euuscapitals.png")

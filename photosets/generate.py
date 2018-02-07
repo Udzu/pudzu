8ec4dd7a7fde3a0dbcecd42e6c14187c96fa5291
Uri Granta <uri.granta@hpe.com>
2018-02-07 09:01:21 +0000

    [pillar] allow non-padded non-upscaled cropped_resized images

--- photosets/generate.py
+++ photosets/generate.py
@@ -1,6 +1,7 @@
 import sys
 sys.path.append('..')
-from charts import *
+import pandas as pd
+from pillar import *
 
 def generate_photoset(path, size=(600,200)):
     df = pd.read_csv(path)
@@ -8,5 +9,5 @@ def generate_photoset(path, size=(600,200)):
     os.makedirs(dirname, exist_ok=True)
     for i,(_,d) in enumerate(df.iterrows(), 1):
         img = Image.from_url_with_cache(d.image)
-        img = img.cropped_resize(size, align=get_non(d, 'align', (0.5,0.25)), pad_up=True, pad_bg="white", pad_align=(0,0.5))
+        img = img.cropped_resize(size, align=get_non(d, 'align', (0.5,0.25)), upsize=False, pad_bg="white", pad_align=(0,0.5))
         img.convert("RGB").save("{}/{}. {}.jpg".format(dirname, i, d.label))

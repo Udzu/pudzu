532d4077d217121ba9ef358760e5587b024e4f7e
Uri Granta <uri.granta@hpe.com>
2018-02-15 15:13:34 +0000

    [dataviz] european jews

--- dataviz/wikifame_eujews.py
+++ dataviz/wikifame_eujews.py
@@ -11,7 +11,9 @@ df = pd.read_csv("datasets/eujews.csv")
 def entitle(img):
     title = Image.from_text("Famous Europeans of Jewish origin ".upper(), FONT(96, bold=True), fg=fg, bg=bg)
     subtitle = Image.from_text("(roughly ordered by how famous they are)", FONT(72), fg=fg, bg=bg).pad((0,0,0,10), bg)
-    return Image.from_column([title, subtitle, img], bg=bg, padding=5).pad(15,bg=bg)
+    img = Image.from_column([title, subtitle, img], bg=bg, padding=5).pad(15,bg=bg)
+    img = img.pad((0,0,0,20), bg).place(Image.from_text("/u/Udzu", FONT(16), fg=fg, bg=bg, padding=5).pad((1,1,0,0), fg), align=1, padding=10)
+    return img
 
 grids = []
 for row in generate_batches(df.iterrows(), ceil(len(df)/4)):
@@ -25,12 +27,12 @@ for row in generate_batches(df.iterrows(), ceil(len(df)/4)):
           Image.from_url_with_cache(get_non(d, 'image', DEFAULT_IMG)).cropped_resize((160,160), (0.5,get_non(d, 'align', 0.2))),
           Image.from_text(d['link'], FONT(16, bold=True), fg=fg, bg=bg),
           Image.from_text(d['description'], FONT(16, italics=True),fg=fg, bg=bg)
-          ], bg=bg, padding=2).pad(10, bg)
+          ], bg=bg, padding=2).pad(5, bg)
         
     def flag(column):
         flag = Image.from_url_with_cache(countries.flag[table.columns[column]]).to_rgba()
         flag = flag.resize_fixed_aspect(height=100) if flag.width / flag.height < 1.3 else flag.resize((160,100))
-        flag = flag.trim(1).pad(1, "grey")
+        flag = flag.trim(1).pad(1, "grey").pad((0,10,0,0), bg)
         return flag
 
     grid = grid_chart(table, cell, col_label=flag, bg=bg)
@@ -38,4 +40,5 @@ for row in generate_batches(df.iterrows(), ceil(len(df)/4)):
     
 chart = Image.from_column(grids, xalign=0)
 chart = entitle(chart)
+chart.convert("RGB").save("output/wikifame_eujews.jpg")
 

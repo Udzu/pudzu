5d9b0fe617573da7852a5b50462dd6cbbd01f5e7
Uri Granta <uri.granta@hpe.com>
2018-03-19 13:29:54 +0000

    [photosets] jewish monarchs

--- photosets/generate.py
+++ photosets/generate.py
@@ -3,7 +3,7 @@ sys.path.append('..')
 import pandas as pd
 from pillar import *
 
-def generate_photoset(path, size=(600,200), credit_fonts=None, credit_overlay=False):
+def generate_photoset(path, size=(600,200), credit_fonts=partial(verdana, 16), credit_overlay=True, credit_bg="black", credit_fg="white"):
     df = pd.read_csv(path)
     dirname = "output/{}".format(os.path.splitext(os.path.basename(path))[0])
     os.makedirs(dirname, exist_ok=True)
@@ -12,11 +12,14 @@ def generate_photoset(path, size=(600,200), credit_fonts=None, credit_overlay=Fa
         aligns = tmap(float, str(d['align']).split('|')) if get_non(d, 'align') else [(0.5, 0.25)]*len(urls)
         imgs = []
         for j, (url, align) in  enumerate(zip_equal(urls, aligns)):
-            img = Image.from_url_with_cache(url)
+            img = Image.from_url_with_cache(url).to_rgba()
             sz = ((size[0]*(j+1))//len(urls)-(size[0]*j)//len(urls), size[1])
             imgs.append( img.cropped_resize(sz, align=align, upsize=False) )
         img = Rectangle(size, "white").place(Image.from_row(imgs, yalign=0), align=0) 
         if credit_fonts:
-            credit = "**Source{}**: {}".format("s"*(len(urls)>1), ", ".join(urlparse(url).netloc.split(".", int(urlparse(url).netloc.count(".") > 1))[-1] for url in urls))
-            img = img.place(Image.from_markup(credit, credit_fonts, "white", "black", padding=(3,1), beard_line=True), align=(0,1))
+            sources = [urlparse(url).netloc.split(".", int(urlparse(url).netloc.count(".") > 1))[-1] for url in urls]
+            if len(set(sources)) == 1: sources = sources[:1]
+            credit = "**Source{}**: {}".format("s"*(len(urls)>1), ", ".join(sources))
+            credit_img = Image.from_markup(credit, credit_fonts, credit_fg, credit_bg, padding=(3,1), beard_line=True)
+            img = img.place(credit_img, align=(0,1)) if credit_overlay else img.pin(credit_img, (0,img.height), align=0, bg=credit_bg)
         img.convert("RGB").save("{}/{}. {}.jpg".format(dirname, i, d.label))

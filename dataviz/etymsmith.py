cadf8afc1e8a5c43f2429e825989aafb3a231495
Uri Granta <uri.granta@hpe.com>
2018-06-14 09:31:50 +0100

    [dataviz] endianess and Smith

---
+++ dataviz/etymsmith.py
@@ -0,0 +1,56 @@
+import sys
+sys.path.append('..')
+from charts import *
+from bamboo import *
+from PIL import ImageEnhance
+
+df = pd.read_csv("datasets/etymsmith.csv").set_index("language").fillna("")
+FONT = arial
+UFONT = partial(font, "fonts/arialu")
+LFONT = lambda l: FONT if l not in ["Georgian", "Armenian", "Arabic", "Hebrew", "Persian"] else UFONT
+
+PALETTE = [VegaPalette10.RED, VegaPalette10.BLUE, VegaPalette10.LIGHTBLUE, VegaPalette10.GREEN, VegaPalette10.LIGHTGREEN, VegaPalette10.ORANGE, VegaPalette10.PINK, VegaPalette10.BROWN, VegaPalette10.GREY]
+CATEGORIES = ["s", "f", "F", "k", "K", "g", "S", "d", ""]
+
+DESCRIPTIONS = [
+" from Proto-Germanic //*smiþaz// (“craftsman, smith”)",
+" from Latin //ferrārius// (“ironsmith”), from //ferrum// (“iron”)",
+" from Latin //faber// (“craftsman”)",
+" from Proto-Slavic //*kovati// (“to forge”)",
+" from Proto-Baltic ??",
+" from Proto-Celtic //*goban// (“smith”)",
+" from Proto-Finnic //*seppä// (“smith”)",
+" from Proto-Turkic //*temürči// (“blacksmith”)",
+" from various other roots"
+]
+HEADER = "Etymology"
+FOOTER = None
+ 
+def colorfn(c):
+    cols = ValueCache()
+    if c in ['Sea', 'Language Borders']: return "white"
+    elif c in ['Country Borders']: return "#AAAAAA"
+    elif c not in df.index: return "grey"
+    elif len(cols << [PALETTE[CATEGORIES.index(x)] for x in df.group.get(c)]) == 1: return cols()[0]
+    elif df.word[c]: return "grey"
+    else: return "#AAAAAA"
+    
+def labelfn(c, w, h):
+    if c not in df.index: return None
+    label = df.word[c].replace("\\n", "\n")
+    return Image.from_text_bounded(label, (w, h), 24, papply(LFONT(c), bold=True), align="center", padding=(0,0,0,2))
+    
+map = map_chart("maps/Eurolang2.png", colorfn, labelfn)
+legend = generate_legend(PALETTE[:len(CATEGORIES)], DESCRIPTIONS, box_sizes=(40,...), header=HEADER, footer=FOOTER, fonts=partial(FONT, 16))
+chart = map.place(legend, align=(1,0))
+
+title = Image.from_column([
+Image.from_text("SMITH IN DIFFERENT EUROPEAN LANGUAGES", FONT(48, bold=True)),
+Image.from_text("the most common forms of the occupational surname", FONT(36))],
+bg="white")
+
+img = Image.from_column([title, chart], bg="white", padding=2)
+img = ImageEnhance.Color(img).enhance(0.7)
+img.place(Image.from_text("/u/Udzu", font("arial", 16), fg="black", bg="white", padding=5).pad((1,1,0,0), "black"), align=1, padding=10, copy=False)
+img.save("output/etymsmith.png")
+
---
+++ dataviz/euendian.py
@@ -0,0 +1,52 @@
+import sys
+sys.path.append('..')
+from charts import *
+from bamboo import *
+from PIL import ImageEnhance
+
+df = pd.read_csv("datasets/euendian.csv").set_index("country")
+
+FONT = arial
+PALETTE = [VegaPalette10.BLUE, VegaPalette10.ORANGE, VegaPalette10.PINK]
+CATEGORIES = ["l", "m", "b"]
+
+def colorfn(c, group):
+    cols = ValueCache()
+    if c in ['Sea', 'Borders']: return "white"
+    elif c not in df.index: return "grey"
+    elif len(cols << [PALETTE[CATEGORIES.index(x)] for x in df[group].get(c)]) == 1: return cols()[0]
+    else: return Stripe(10, cols()[0], cols()[1])
+    
+date_eu = map_chart("maps/Europe.png", partial(colorfn, group="date"))
+date_us = map_chart("maps/USA.png", lambda c: "white" if c in ['Sea', 'Borders'] else PALETTE[CATEGORIES.index("m")]).trim((0,0,50,0))
+date_legend = generate_legend(PALETTE, [
+"Little endian: e.g. 14.07.1789",
+"Middle endian: e.g. 07.14.1789",
+"Big endian: e.g. 1789.07.14"
+], header="Year/Month/Day ordering".upper(), fonts=partial(FONT, 32))
+date_map = Image.from_row([date_us, date_eu.place(date_legend, align=(1,0), padding=10)], bg="white")
+
+address_eu = map_chart("maps/Europe.png", partial(colorfn, group="address"))
+address_us = map_chart("maps/USA.png", lambda c: "white" if c in ['Sea', 'Borders'] else PALETTE[CATEGORIES.index("l")]).trim((0,0,50,0))
+address_legend = generate_legend(PALETTE, [
+"Little-endian: e.g. 10 Downing Street, London",
+"Middle-endian: e.g. Prinsengracht 263, Amsterdam",
+"Big-endian: e.g. Budapest, Dísz tér 11"
+], header="City/Street/Number ordering".upper(), fonts=partial(FONT, 32))
+address_map = Image.from_row([address_us, address_eu.place(address_legend, align=(1,0), padding=10)])
+
+date_title = Image.from_column([
+Image.from_text("DATE FORMAT ENDIANNESS", FONT(96, bold=True)), 
+Image.from_text("(preferred format according to ICU locales)", FONT(54, italics=True))
+], padding=5)
+
+address_title = Image.from_column([
+Image.from_text("ADDRESS FORMAT ENDIANNESS", FONT(96, bold=True)), 
+Image.from_text("(preferred format according to the Universal Postal Union)", FONT(54, italics=True))
+], padding=5)
+
+img = Image.from_column([date_title, date_map, address_title, address_map], bg="white", padding=5)
+img = ImageEnhance.Color(img).enhance(0.7)
+img.place(Image.from_text("/u/Udzu", font("arial", 24), fg="black", bg="white", padding=5).pad((1,1,0,0), "black"), align=1, padding=10, copy=False)
+img.save("output/euendian.png")
+

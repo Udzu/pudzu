128cb7165da100fe48f56789508d82ca89f2b19a
Uri Granta <uri.granta@microfocus.com>
2018-11-09 11:21:56 +0000

    [dataviz] weighted flags

--- dataviz/flagsaverage4.py
+++ dataviz/flagsaverage4.py
@@ -3,7 +3,14 @@ from pudzu.bamboo import *
 import scipy.stats
 
 df = pd.read_csv("../dataviz/datasets/countries.csv").filter_rows("organisations >> un").split_columns('country', "|").split_rows('country').set_index('country').drop_duplicates(subset='flag', keep='first')
-order = ['GULES', 'ARGENT', 'AZURE', 'VERT', 'OR', 'CELESTE', 'SABLE', 'TENNE', 'ORANGE', 'PURPURE']
+
+wf = pd.read_html("https://en.wikipedia.org/wiki/List_of_countries_and_dependencies_by_area")[0]
+wf = wf.rename(columns=wf.iloc[0])[1:].fillna("0")
+wf = wf.assign_rows(country=lambda d: d[next(c for c in wf.columns if "state" in c)].split(" (")[0].split(" !")[-1].strip(" \xa0"))
+wf = wf.assign_rows(area=lambda d: float(d[next(c for c in wf.columns if "Total" in c)].split(" (")[0].split("(")[0].split(chr(9824))[-1].replace(",","").replace("<","")))
+wf = wf.drop_duplicates("country", "last").set_index("country")
+wf.area["Micronesia"] = wf.area["Federated States of Micronesia"]
+df = df.assign_rows(area=lambda d,c: wf.area.get(c) or wf.area[c.split(" ")[-1]])
 
 class HeraldicPalette(metaclass=NamedPaletteMeta):
     OR = "#fcdd09"
@@ -27,12 +34,18 @@ def histogram(c):
 
 df['histogram'] = [histogram(c) for c in df.index]
 
-def averaged(continent=None, weighted=False):
-    histograms = [valmap(lambda v: v*(r.population if weighted else 1), r.histogram) for _,r in df.iterrows() if continent is None or r.continent == continent]
+def by_country(i, r, c): return [1,0][c == "Asia"] if i == "Russia" and c is not None else 1
+def by_population(i, r, c): return [110000000,35000000][c == "Asia"] if i == "Russia" and c is not None else r.population
+def by_area(i, r, c):return [4275000,12825000][c == "Asia"] if i == "Russia" and c is not None else r.area
+    
+def averaged(continent=None, weight=by_area):
+    histograms = [valmap(lambda v: v*weight(c, r, continent), r.histogram) for c,r in df.iterrows() if continent is None or r.continent == continent or c == "Russia" and continent == "Asia"]
     average = merge_dicts(*histograms, merge_fn=lambda k,*vs: sum(vs))
     average = valmap(lambda v: v / sum(average.values()), average)
-    average = sorted(average.items(), key=lambda s: order.index(s[0]))
+    average = sorted(average.items(), key=lambda s: -s[1] if continent is None else order.index(s[0]))
     return average
+
+order = [x[0] for x in averaged()]
 
 def diff_histograms(h1, h2):
     return sum(abs(h1.get(c,0) - h2.get(c,0)) for c in HeraldicPalette.names)
@@ -57,11 +70,11 @@ grid = Image.from_array(array, padding=5, bg="white", xalign=(1,0.5,0.5,0))
 allflags = [Image.from_url_with_cache(df.flag[c]).convert("RGBA").resize((30,20)) for c in sorted(df.index)]
 allflags = Image.from_array(list(generate_batches(allflags, 28)))
 
-title = Image.from_text("Most representative flag per continent".upper(), calibri(48, bold=True))
-label1 = Image.from_text("calculated by combining the colors of all the relevant flags:", calibri(24, italics=True))
+title = Image.from_text("Most representative flag (weighted)".upper(), calibri(48, bold=True))
+label1 = Image.from_text("calculated by combining the colors of all the relevant flags, weighted by area:", calibri(24, italics=True))
 label2 = Image.from_text("quantizing them to heraldic colors:", calibri(24, italics=True))
 checkers = Checkers((40*len(HeraldicPalette), 40), GradientColormap(*HeraldicPalette), shape=(len(HeraldicPalette), 1), colors=len(HeraldicPalette)).add_grid((len(HeraldicPalette), 1))
 label3 = Image.from_text("and then finding the flag with the most similar color composition.", calibri(24, italics=True))
 
 img = Image.from_column([title, label1, allflags, label2, checkers, label3, Rectangle((0,20)), grid], padding=5, bg="white")
-img.save("output/flagsaverage4.png")
+img.save("output/flagsaverage4_area.png")

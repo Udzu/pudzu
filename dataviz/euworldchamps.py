2a13e5b651a6348b21913e264cc88d954f434eea
Udzu <uri.zarfaty@gmail.com>
2019-07-15 19:44:13 +0100

    [dataviz] more eu world champs

--- dataviz/euworldchamps.py
+++ dataviz/euworldchamps.py
@@ -1,7 +1,6 @@
 from pudzu.charts import *
 from pudzu.bamboo import *
 
-# generate map
 df = pd.read_csv("datasets/euworldchamps.csv").set_index("sport").split_columns(["men", "women"], "|")
 FONT = calibri
 
@@ -11,7 +10,16 @@ EXCOUNTRIES = { "USSR": { "Russia", "Latvia", "Estonia", "Lithuania", "Belarus",
 "UK": { "England", "Scotland", "Wales", "Northern Ireland"}
 }
 SPORTS = list(df.index)
-PALETTE = VegaPalette10
+PALETTE = [VegaPalette10.RED, VegaPalette10.BLUE, VegaPalette10.GREEN, VegaPalette10.ORANGE, VegaPalette10.PURPLE, VegaPalette10.LIGHTGREEN, VegaPalette10.BROWN, VegaPalette10.PINK]
+SIZE = 25
+
+def fade(color):
+    return RGBA(color).blend("#AAAAAA", 0.5)
+    
+def pattern(colors):
+    p = Image.from_column([Rectangle([10,(SIZE*(i+1))//len(colors)-(SIZE*i)//len(colors)], c) for i,c in enumerate(colors)])
+    return p
+    #return Image.from_pattern(p, (2000, 2000)).rotate(30, Image.BILINEAR).crop((500,500,1500,1500))
 
 def colorfn(c, *args, men=True):
     print(c)
@@ -19,9 +27,17 @@ def colorfn(c, *args, men=True):
     colors = []
     for sport, winners in df['men' if men else 'women'].iteritems():
         exwinners = { x for c in winners if c in EXCOUNTRIES for x in EXCOUNTRIES[c] }
-        if c in winners or c in exwinners and c in EXCOUNTRIES["UK"]: colors.append(VegaPalette10[SPORTS.index(sport)])
-        elif c in exwinners: colors.append(VegaPalette10[SPORTS.index(sport)].blend("grey", 0.6))
-    if len(colors) == 0: return "grey"
-    else: return Image.from_column([Rectangle([10,10], c) for c in colors])
+        if c in winners or c in exwinners and c in EXCOUNTRIES["UK"]: colors.append(PALETTE[SPORTS.index(sport)])
+        elif c in exwinners: colors.append(fade(PALETTE[SPORTS.index(sport)]))
+    if len(colors) == 0: return "#AAAAAA"
+    return pattern(colors)
     
-map = map_chart("maps/EuropeUK.png", colorfn)
+map1 = map_chart("maps/EuropeUK.png", colorfn)
+map2 = map_chart("maps/EuropeUK.png", partial(colorfn, men=False))
+
+legend = generate_legend([Image.from_row([Rectangle(40, c)]) for c in PALETTE],
+                         [sport.title() for sport in SPORTS], font_family=partial(FONT, size=16),
+                         header="Sport", footer="faded colors correspond to ex countries", max_width=150)
+chart1 = map1.place(legend, align=(1,0), padding=10)
+chart2 = map2.place(legend, align=(1,0), padding=10)
+chart = Image.from_row([chart1, chart2])

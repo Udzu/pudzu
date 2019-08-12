316d24b0ff8be862734e7d1624fcdfb5ab5c2610
Udzu <uri.zarfaty@gmail.com>
2019-08-12 19:43:55 +0100

    [dataviz] fix up brazil police map

--- dataviz/politics_brazil.py
+++ dataviz/politics_brazil.py
@@ -3,6 +3,7 @@ from pudzu.charts import *
 table = pd.read_html("https://en.wikipedia.org/wiki/List_of_countries_by_intentional_homicide_rate")[1]
 df = table.drop_duplicates().set_index(0)[[3]].dropna().rename(columns={3: 'homicides'})[1:]
 df = df[df.homicides != "Rate"].update_columns(homicides=float).sort_values("homicides", ascending=False)
+greater = []
 
 def colorfn(c):
     c = { "Republic of the Congo": "Congo", 
@@ -13,14 +14,18 @@ def colorfn(c):
           "Aland Islands": "Finland",
           "Western Sahara": "Morocco"}.get(c, c)
     if c in ['Sea', 'Borders']: return "white"
-    if c not in df.index: return print(c) or VegaPalette10.RED
-    #if df.homicides[c] * 2 < 2.93: return VegaPalette10.RED
-    if df.homicides[c] < 2.93: return VegaPalette10.RED
-    #if df.homicides[c] / 2 < 2.93: return VegaPalette10.LIGHTBLUE
+    if c not in df.index or df.homicides[c] < 2.93:
+        greater.append(c)
+        return VegaPalette10.RED
     else: return "grey"
 
 chart = map_chart("maps/World.png", colorfn)
-title = Image.from_markup("**Countries whose overall intentional homicide rate is lower than the Brazilian police's 2018 homicide rate**".upper(), partial(arial, 40))
+title = Image.from_markup("**Countries whose overall intentional homicide rate is lower than the Brazilian police’s 2018 homicide rate**".upper(), partial(arial, 40))
 img = Image.from_column([title, chart], bg="white", padding=5)
 img.place(Image.from_text("/u/Udzu", font("arial", 16), fg="black", bg="white", padding=5).pad((1,1,0,0), "black"), align=1, padding=10, copy=False)
 img.save("output/politics_brazil.png")
+
+# TODO: use d3 directly for anti-aliasing
+# from generate import *
+# generate_datamap("brazil_pc", dict_from_vals(greater=greater), { "greater": VegaPalette10.RED })
+# chart = Image.open("temp/brazil_pc.png")

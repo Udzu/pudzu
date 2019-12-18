246a0ca78197cebc237826215fa311ea43c41468
Udzu <uri.zarfaty@gmail.com>
2019-12-18 20:55:30 +0000

    [unicode] Fix Unicode script to handle some property files

--- pudzu/sandbox/unicode/unicode.py
+++ pudzu/sandbox/unicode/unicode.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 from pudzu.utils import *
 
-UCD_COLUMNS = [
+UNICODEDATA_COLUMNS = [
     "Code_Point",
     "Name",
     "General_Category",
@@ -23,10 +23,11 @@ UCD_COLUMNS = [
 ]
 
 
-def ucd_data():
+def extract_unicodedata():
+    """ Convert UnicodeData.txt into a DataFrame. """
 
     with importlib.resources.open_text(__package__, "UnicodeData.txt") as fh:
-        df = pd.read_csv(fh, sep=";", header=None, names=UCD_COLUMNS, index_col="Code_Point",
+        df = pd.read_csv(fh, sep=";", header=None, names=UNICODEDATA_COLUMNS, index_col="Code_Point",
                          converters={'Code_Point': artial(int, 16)})
 
     # remove Surrogate and Private Use characters
@@ -37,26 +38,30 @@ def ucd_data():
         df = df.reindex(df.index.append(pd.Index(range(a+1,z))).sort_values(), method='pad')
         df.loc[a:z, "Name"] = (
                 df.loc[a:z].Name.str.replace(re.compile(", (First|Last)>"), "") + ", #" +
-                df.loc[a:z].index.to_series().apply(artial(format, 'x')) + ">")
+                df.loc[a:z].index.to_series().apply(artial(format, '04x')) + ">")
 
+    df["Code_Point"] = df.index.to_series().apply(artial(format, '04x'))
     return df
 
 
-def additional_data(filename, properties):
+def extract_property(filename, property):
+    """ Convert a standard UCD file. Currently only handles a subset of enumerated and binary properties. """
 
     with importlib.resources.open_text(__package__, filename) as fh:
         df = pd.read_csv(fh, sep=";", header=None, skip_blank_lines=True, comment="#",
-                         names=['Code_Point'] + properties)
+                         names=['Code_Point', property])
 
     # remove whitespace and set (possibly non-unique) index
     df = df.apply(lambda x: x.str.strip() if x.dtype == "object" else x)
     df.index = df.Code_Point.str.replace("\.\..*","").apply(artial(int, 16))
+    # TODO: filter out Surrogate and Private Use characters
 
     # expand code ranges
     expanded = df
-    for i, az in enumerate(df[df.Code_Point.str.contains("\.\.")].Code_Point):
+    ranges = df[df.Code_Point.str.contains("\.\.")]
+    for i, az in enumerate(ranges.Code_Point):
         a, z = map(artial(int, 16), az.split(".."))
-        copies = pd.DataFrame([df.iloc[i]]*(z-a), index=range(a+1,z+1))
+        copies = pd.DataFrame([ranges.iloc[i]]*(z-a), index=range(a+1,z+1))
         expanded = pd.concat([expanded, copies])
 
     # combine properties
@@ -70,8 +75,9 @@ def additional_data(filename, properties):
 
 def unicode_data():
     # TODO: logging
-    df = ucd_data()
-    scripts = additional_data("Scripts.txt", ["Script"])
-    emoji = additional_data("emoji-data.txt", ["Emoji"])
+    df = extract_unicodedata()
+    scripts = extract_property("Scripts.txt", "Script")
+    emoji = extract_property("emoji-data.txt", "Emoji")
+    # TODO: PropList, Blocks
     df = pd.concat([df, scripts, emoji], axis=1)
     return df

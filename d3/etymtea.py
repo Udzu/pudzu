fc94332825ca58c5546953f848ed2409145a26bc
Udzu <uri.zarfaty@gmail.com>
2020-02-20 15:13:20 +0000

    [d3] more tea please

--- d3/etymtea.py
+++ d3/etymtea.py
@@ -4,12 +4,36 @@ from generate import *
 
 FONT = sans
 DEFAULT = VegaPalette10.GREEN
-PALETTE = { "t": VegaPalette10.BLUE, "c": VegaPalette10.GREEN, "ot": VegaPalette10.LIGHTBLUE, "oc": VegaPalette10.LIGHTGREEN, "o": VegaPalette10.RED }
+PALETTE = { 
+"t": VegaPalette10.RED, 
+"ot": VegaPalette10.PINK,
+"c": VegaPalette10.BLUE,
+"oc": VegaPalette10.LIGHTBLUE,
+"o": VegaPalette10.BROWN,
+ }
+PATTERNS = ""
 
 df = pd.read_csv("datasets/etymtea.csv").set_index("country")
 
-assert set(df.index) < set(load_name_csv("../dataviz/maps/World.png").name), f'Unrecognised countris: {set(df.index) - set(load_name_csv("../dataviz/maps/World.png").name)}'
+# TODO: move this logic elsewhere
+WIDTH = 4
+for c, r in df.iterrows():
+    p = r.category
+    if isinstance(p, str) and p not in PALETTE:
+        ps = p.split("|")
+        if all(p in PALETTE for p in ps):
+            rects = [f'<rect x="{i*WIDTH}" y="0" width="{WIDTH}" height="{WIDTH*len(ps)}" style="fill:{PALETTE[p].to_hex()}"/>' for i,p in enumerate(ps)]
+            PATTERNS += f'''<defs>
+  <pattern id="{''.join(ps)}" width="{WIDTH*len(ps)}" height="{WIDTH*len(ps)}" patternTransform="rotate(45 0 0)" patternUnits="userSpaceOnUse">
+    {'  '.join(rects)}
+  </pattern>
+</dev>
+'''
+            PALETTE[p] = f'url(#{"".join(ps)})'
+    if p not in PALETTE: print(c, p)
+
+assert set(df.index) < set(atlas.index), f'Unrecognised countries: {set(df.index) - set(load_name_csv("../dataviz/maps/World.png").name)}'
 
 # TODO: stripes
-colormap = { c: PALETTE.get(r.category, VegaPalette10.RED) for c,r in df.iterrows() }
-generate_datamap("etymtea", colormap, codifier=partial(codify_countries, dependencies=False))
+colormap = { c: df.category[c] if df.category[c] in PALETTE else "o" for c in df.index }
+generate_datamap("etymtea", colormap, palette=PALETTE, patterns=PATTERNS, codifier=partial(codify_countries, dependencies=False))

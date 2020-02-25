cde4a75adb295b10201e3ac5276178ddfd1404f9
Udzu <uri.zarfaty@gmail.com>
2020-02-25 21:10:40 +0000

    [patterns] handle kleene and start parser

--- pudzu/sandbox/patterns.py
+++ pudzu/sandbox/patterns.py
@@ -1,16 +1,18 @@
-from pudzu.utils import *
-from typing import *
 import copy
+from functools import reduce
+from typing import *
+from pudzu.utils import *
 
 State = Union[int, Collection['State']]
 Move = Enum('Move', 'EMPTY ALL')
 Input = Union[str, Move]
 Transitions = Dict[Tuple[State, Input], AbstractSet[State]]
 
+
 class NFA:
     """Nondeterministic Finite Automata with
     - single start and end state
-    - ε-moves (including potentially infinite ε loops)
+    - ε-moves (including potential ε loops)
     - *-moves (only used if there is no other matching move)
     """
 
@@ -39,37 +41,60 @@ class NFA:
     def remove_unreachable_states(self) -> None:
         ...
 
+        
 def MatchIn(characters: str) -> NFA:
-    """Examples: a, [abc]"""
+    """Handles: a, [abc]"""
     return NFA(0, 1, {(0, c): {1} for c in characters})
 
 def MatchNotIn(characters: str) -> NFA:
-    """Examples: [^abc], ."""
+    """Handles: [^abc], ."""
     return NFA(0, 1, merge({(0, Move.ALL): {1}}, {(0, c): set() for c in characters}))
 
 def MatchAfter(nfa1: NFA, nfa2: NFA) -> NFA:
-    """Examples: ab"""
+    """Handles: ab"""
     t1 = {((s,1),i): {(t,1) for t in ts} for (s,i),ts in nfa1.transitions.items()}
     t2 = {((nfa1.end, 1) if s == nfa2.start else (s,2),i): {(t,2) for t in ts} for (s,i),ts in nfa2.transitions.items()}
     return NFA((nfa1.start, 1), (nfa2.end, 2), merge(t1, t2))
     
 def MatchEither(nfa1: NFA, nfa2: NFA) -> NFA:
-    """Examples: a|b"""
+    """Handles: a|b"""
     t1 = {((s,1),i): {(t,1) for t in ts} for (s,i),ts in nfa1.transitions.items()}
     t2 = {((s,2),i): {(t,2) for t in ts} for (s,i),ts in nfa2.transitions.items()}
     t12 = {(0, Move.EMPTY): {(nfa1.start, 1), (nfa2.start, 2)}, ((nfa1.end, 1), Move.EMPTY): {1}, ((nfa2.end, 2), Move.EMPTY): {1}}
     return NFA(0, 1, merge(t1, t2, t12))
 
-def MatchOptional(nfa: NFA) -> NFA:
-    """Examples: a?"""
+def MatchRepeated(nfa: NFA, repeat: bool, optional: bool) -> NFA:
+    """Handles: a*, a+, a?"""
     transitions = copy.deepcopy(nfa.transitions)
-    transitions.setdefault((nfa.start, Move.EMPTY), set()).add(nfa.end)
+    if optional: transitions.setdefault((nfa.start, Move.EMPTY), set()).add(nfa.end)
+    if repeat: transitions.setdefault((nfa.end, Move.EMPTY), set()).add(nfa.start)
     return NFA(nfa.start, nfa.end, transitions)
 
-def MatchKleene(nfa: NFA) -> NFA:
-    """Examples: a*"""
-    ...
 
-# TODO: MatchBoth, MatchRepeated, etc
+class Pattern:
+    from pyparsing import Word, Optional, oneOf, Forward, OneOrMore, alphas, Group, Literal
+    
+    # TODO: parser actions, whitespace handling, braces, operators, unicode, escaping
+    literal = Word(alphas, exact=1).setParseAction(lambda t: MatchIn(t[0]))
+    dot = Literal(".").setParseAction(lambda t: MatchNotIn(""))
+    set = ("[" + Word(alphas, min=1) + "]").setParseAction(lambda t: MatchIn(t[1]))
+    negset = ("[^" + Word(alphas, min=1) + "]").setParseAction(lambda t: MatchNotIn(t[1]))
+    postfix = oneOf("?+*")
+    
+    expr = Forward()
+    group = Group("(" + expr + ")")
+    atom = literal | dot | set | negset | group
+    item = atom + Optional(postfix)
+    items = OneOrMore(item).setParseAction(lambda t: reduce(MatchAfter, t))
+    
+    expr <<= items
 
-# TODO: parser
+    def __init__(self, pattern: str):
+        self.pattern = pattern
+        self.nfa = self.expr.parseString(pattern)[0]
+
+    def __repr__(self):
+        return f"Pattern({self.pattern!r})"
+        
+    def match(self, string: str) -> bool:
+        return self.nfa.match(string)

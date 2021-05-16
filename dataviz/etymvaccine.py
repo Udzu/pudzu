3a62fb7c2b8921dfc5af7db3621d9d62e655ed11
Udzu <uri.zarfaty@gmail.com>
2021-05-16 16:04:24 +0100

    [dataviz] start vaccine etymology map

---
+++ dataviz/etymvaccine.py
@@ -0,0 +1,49 @@
+from pudzu.charts import *
+from pudzu.sandbox.bamboo import *
+from PIL import ImageEnhance
+
+df = pd.read_csv("datasets/etymvaccine.csv").set_index("language")
+df["group"] = df["group"].fillna("v")
+df["word"] = df["word"].fillna(" ")
+FONT = arial
+UFONT = partial(font, "fonts/arialu")
+LFONT = lambda l: FONT if l not in ["Georgian", "Armenian", "Arabic", "Hebrew", "Persian"] else UFONT
+
+PALETTE = [PairedClass12.RED, PairedClass12.BLUE, PairedClass12.GREEN ]
+CATEGORIES = ["v", "a", "o"]
+
+DESCRIPTIONS = [
+"""vaccine""",
+"""other1""",
+"""other2""",
+]
+ 
+def colorfn(c):
+    cols = ValueBox()
+    if c in ['Sea', 'Language Borders']: return "white"
+    elif c in ['Country Borders']: return "#AAAAAA"
+    elif c not in df.index: return "grey"
+    elif (df.word.get(c) == " "): return "grey"
+    elif len(cols << [PALETTE[CATEGORIES.index(x)] for x in df.group.get(c)]) == 1: return cols()[0]
+    else: return Stripe(20, cols()[0], cols()[1])
+    
+def labelfn(c, w, h):
+    if c not in df.index: return None
+    label = df.word[c].replace("\\n", "\n")
+    return Image.from_text_bounded(label, (w, h), 24, papply(LFONT(c), bold=True), align="center", padding=(0,0,0,2))
+    
+map = map_chart("maps/Eurolang2.png", colorfn, labelfn)
+legend = generate_legend(PALETTE[:len(CATEGORIES)], DESCRIPTIONS, box_sizes=(40,...), font_family=partial(FONT, 16))
+chart = map.place(legend, align=(1,0))
+
+title = Image.from_column([
+Image.from_text("VACCINE IN DIFFERENT EUROPEAN LANGUAGES", FONT(48, bold=True)),
+# Image.from_text("decimal versus vigesimal counting systems", FONT(36))
+],
+bg="white")
+
+img = Image.from_column([title, chart], bg="white", padding=2)
+img = ImageEnhance.Color(img).enhance(0.7)
+img.place(Image.from_text("/u/Udzu", font("arial", 16), fg="black", bg="white", padding=5).pad((1,1,0,0), "black"), align=1, padding=10, copy=False)
+img.save("output/etymvaccine.png")
+
